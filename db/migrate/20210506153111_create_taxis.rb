96719405b952d3c6d9950dac02e445121f68966f
Hipppie3 <107952593+Hipppie3@users.noreply.github.com>
2022-11-28 11:44:51 -0700

    commit tree 9c50bbe5ac8707ad89a6836a6694b9f474db7f5f

--- db/migrate/20210506153111_create_taxis.rb
+++ db/migrate/20210506153111_create_taxis.rb
@@ -1,6 +1,9 @@
 class CreateTaxis < ActiveRecord::Migration[6.1]
   def change
     create_table :taxis do |t|
+      t.string  :name
+      t.string  :color
+      t.integer :number
 
       t.timestamps
     end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+p1 = Passenger.create(name: "D", age: 2)
+p2 = Passenger.create(name: "A", age: 5)
+p3 = Passenger.create(name: "N", age: 10)
+
+t1 = Taxi.create(name: "Honda", color: "White", number: 2000)
+t2 = Taxi.create(name: "Toyota", color: "Black", number: 4)
+t3 = Taxi.create(name: "Mazda", color: "Red", number: 7)
+
+Ride.create(passenger_id: p1.id, taxi_id: t1.id)
+Ride.create(passenger_id: p2.id, taxi_id: t2.id)
+Ride.create(passenger_id: p3.id, taxi_id: t3.id)
+Ride.create(passenger_id: p1.id, taxi_id: t3.id)
+Ride.create(passenger_id: p2.id, taxi_id: t1.id)
